bbeee2db6c23f17ae4e9c419af7e103a692c4293
8area8 <mbriolet.ma@gmail.com>
2018-10-15 13:40:43 +0200

    autocompletion implemented. Have to fixe the 2 inputs bug, and implement the last task.

--- assets/dev/default/js/autocomplete.js
+++ assets/dev/default/js/autocomplete.js
@@ -1,18 +1,43 @@
 import '../../vendor/autocomplete/jquery-ui.scss';
 import '../../vendor/autocomplete/jquery-ui.js';
 
-var products;
-var jqxhr = $.get(window.product_names_url, function () {
-})
-    .done(function () {
-        console.log("success");
-        products = JSON.parse(jqxhr.responseText).resp;
-        $(function () {
-            $("input").autocomplete({
-                source: products.slice(50)
-            });
-        })
-    })
-    .fail(function () {
-        console.log("error");
-    });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+});
+
+$('input').autocomplete({
+    source: function (req, response) { // les deux arguments représentent les données nécessaires au plugin
+        $.ajax({
+            type: "POST",
+            url: window.product_names_url, // on appelle le script JSON
+            dataType: 'json', // on spécifie bien que le type de données est en JSON
+            data: {
+                starts_with: $('#input').val(), // on donne la chaîne de caractère tapée dans le champ de recherche
+                max_len: 15
+            },
+
+            success: function (datas) {
+                console.log("success")
+                response(datas.names)
+            }
+        });
+    }
+});
--- django_apps/autocomplete/views.py
+++ django_apps/autocomplete/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -15,6 +17,12 @@ def redis_names(request):
 
 def ajax_product_names(request):
     """Return the product names."""
-    products = redis.smembers("product_names")
-    products_list = [product.decode("utf-8") for product in products]
-    return JsonResponse({"resp": products_list})
+    starts_with = request.POST.get("starts_with")
+    max_len = int(request.POST.get("max_len"))
+
+    names = redis.smembers("product_names")
+    decoded = (name.decode("utf-8") for name in names)
+    filtered = (name for name in decoded if starts_with in name)
+    sample = [name for index, name in enumerate(filtered) if index < max_len]
+
+    return JsonResponse({"names": sample})
