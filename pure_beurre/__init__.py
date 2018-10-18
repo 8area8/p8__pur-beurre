f63794a022c8af8887e86eb275e7e449c970b4bd
8area8 <mbriolet.ma@gmail.com>
2018-10-18 17:05:20 +0200

    solving redis conf #2

--- pure_beurre/__init__.py
+++ pure_beurre/__init__.py
@@ -5,6 +5,10 @@ from __future__ import absolute_import, unicode_literals
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
-from .redis import app as redis_app
+from django.conf import settings
 
-__all__ = ('celery_app', 'redis_app')
+if settings.DEBUG:
+    from .redis import app as redis_app
+    __all__ = ('celery_app', 'redis_app')
+
+__all__ = ('celery_app')
