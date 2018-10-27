17e0b628cccb99fa9c8b4606821a61edfa86adc7
8area8 <mbriolet.ma@gmail.com>
2018-10-27 17:19:07 +0200

    fixed celery app init.

--- app/celery.py
+++ app/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-app = Celery('pure_beurre')
+app = Celery('app')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
