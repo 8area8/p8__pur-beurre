688a1567d74c4873fce336a5658ceff655fe5c56
8area8 <mbriolet.ma@gmail.com>
2018-10-31 13:41:31 +0100

    minirefactoring.

--- apps/login/tests.py
+++ apps/login/tests.py
@@ -9,7 +9,7 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.urls import reverse
 
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 from .token import account_activation_token
 
