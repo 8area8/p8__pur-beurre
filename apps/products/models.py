4320f627f235f84673bb03b6b4474cb5cbb9af2a
8area8 <mbriolet.ma@gmail.com>
2018-10-31 14:11:16 +0100

    products app created.

---
+++ apps/products/models.py
@@ -0,0 +1,10 @@
+"""Products models."""
+
+from django.db import models
+
+
+class Product(models.Model):
+    """Product class."""
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
