4a270702f689982143f8cba098ecd7ac344248b1
8area8 <mbriolet.ma@gmail.com>
2019-03-01 18:48:18 +0100

    s3 try

--- app/settings.py
+++ app/settings.py
@@ -262,9 +262,16 @@ except ImportError:
     AWS_STORAGE_BUCKET_NAME = os.getenv("BUCKET_NAME")
     AWS_ACCESS_KEY_ID = os.getenv("S3_ACCESS_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("S3_ACCESS_SECRET")
-    AWS_S3_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+    AWS_S3_CUSTOM_DOMAIN = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
     AWS_S3_HOST = "s3-eu-west-1.amazonaws.com"
 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+
+    AWS_LOCATION = 'static'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     DEFAULT_FILE_STORAGE = 'app.storage.MediaStorage'
 
 
