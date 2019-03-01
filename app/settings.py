1571f36e0eecaa0aa687eab018dc0b24f5c05c9d
8area8 <mbriolet.ma@gmail.com>
2019-03-01 18:53:45 +0100

    ntry

--- app/settings.py
+++ app/settings.py
@@ -269,9 +269,9 @@ except ImportError:
         'CacheControl': 'max-age=86400',
     }
 
-    AWS_LOCATION = 'static'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    # AWS_LOCATION = 'static'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     DEFAULT_FILE_STORAGE = 'app.storage.MediaStorage'
 
 
