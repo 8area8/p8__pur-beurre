3c734ab33186ac33173aeb41d380782b6122df17
8area8 <mbriolet.ma@gmail.com>
2019-03-01 18:38:26 +0100

    s3 try

--- app/storage.py
+++ app/storage.py
@@ -1,10 +1,8 @@
 """S3 Storage module."""
 
-from storages.backends.s3boto import S3BotoStorage
-from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class MediaStorage(S3BotoStorage):
-    """Media storage class."""
-
-    location = settings.AWS_MEDIA_DIR
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
